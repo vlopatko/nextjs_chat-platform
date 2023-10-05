7e630d22463594394c695aa9bdbc7f4efdd3b5d8
Valerii Lopatko <vlopatko1999@gmail.com>
2023-10-05 14:29:14 +0300

    added Home page & "shadcn" library & "lucide" library and added styles to tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,8 +17,15 @@ export default function RootLayout({
   children: React.ReactNode
 }): JSX.Element {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body
+        className={cn(
+          'grainy min-h-screen font-sans antialiased',
+          inter.className
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
