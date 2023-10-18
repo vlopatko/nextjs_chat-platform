e2ffe2db983cf4d1070efd62008a44781ca687e7
Valerii Lopatko <vlopatko1999@gmail.com>
2023-10-18 14:01:31 +0300

    added auth middleware for Kinde & NavBar for unauthorized users & Stripe webhooks endpoints

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server'
 import { cn } from '@/lib/utils'
 import { ArrowRight } from 'lucide-react'
+import UserAccountNav from './UserAccountNav'
 
 const NavBar = () => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
   return (
     <nav className="sticky inset-x-0 top-0 z-30 h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -18,36 +26,59 @@ const NavBar = () => {
           {/* todo: add mobile NavBar*/}
 
           <div className="hidden items-center space-x-4 sm:flex">
-            <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({
-                  variant: 'ghost',
-                  size: 'sm',
-                })}
-              >
-                Pricing
-              </Link>
-              <LoginLink
-                className={cn(
-                  buttonVariants({
+            {!user ? (
+              <>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({
                     variant: 'ghost',
                     size: 'sm',
-                  })
-                )}
-              >
-                Sign in
-              </LoginLink>
-              <RegisterLink
-                className={cn(
-                  buttonVariants({
+                  })}
+                >
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={cn(
+                    buttonVariants({
+                      variant: 'ghost',
+                      size: 'sm',
+                    })
+                  )}
+                >
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={cn(
+                    buttonVariants({
+                      size: 'sm',
+                    })
+                  )}
+                >
+                  Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    variant: 'ghost',
                     size: 'sm',
-                  })
-                )}
-              >
-                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
-              </RegisterLink>
-            </>
+                  })}
+                >
+                  Dashboard
+                </Link>
+                <UserAccountNav
+                  email={user.email ?? ''}
+                  imageUrl={user.picture ?? ''}
+                  name={
+                    !user.given_name || !user.family_name
+                      ? 'Your Account'
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                />
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
